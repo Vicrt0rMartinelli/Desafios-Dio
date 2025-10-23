db41380ce60e75cf2349107e184bdd18d493b180
Vicrt0rMartinelli <josevictordasm17112006@gmail.com>
2025-10-23 20:32:36 -0300

    as

--- ContaBanco/src/ContaTerminal.java
+++ ContaBanco/src/ContaTerminal.java
@@ -8,16 +8,16 @@ public static void main(String [] args){
 
     Scanner scanner = new Scanner (System.in).useLocale(brasil);
 
-    System.out.println("Nome: ");
+    System.out.println("Por favor, digite o seu Nome: ");
     String nome = scanner.nextLine();
 
-    System.out.println("Numero da agencia: ");
+    System.out.println("Por favor, digite o número da Agência !");
     String agencia= scanner.nextLine();
 
-    System.out.println("Numero da conta: ");
+    System.out.println("Por favor, digite o numero da conta: ");
     int conta= scanner.nextInt();
 
-    System.out.println("Saldo: (separar com virgula para separação decimal !!!) ");
+    System.out.println("Por favor, digite o Saldo (separar com virgula para separação decimal !!!) ");
     Double saldo = scanner.nextDouble(); 
 
     System.out.printf("Olá %s obrigado por criar uma conta em nosso banco, sua agência é %s, %d e seu saldo R$ %.2f já está disponível para saque",nome, agencia , conta, saldo);
